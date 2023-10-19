6f30a75112ff1cd7588678a5786466ea07372d22
Kevin Esquivel <dev.kevinesquivel@gmail.com>
2023-10-18 20:51:43 -0500

    fix(user): fix a bug in a code line

--- src/main/java/kaeh/personal/loginjwt/controller/UserController.java
+++ src/main/java/kaeh/personal/loginjwt/controller/UserController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class UserController {
 
-    private UserService userService;
+    private final UserService userService;
 
     @GetMapping("/{username}")
     public User getUserInfo(@PathVariable String username) {
